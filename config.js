8f95a7cf5ec68625478913c4025892e977070274
BambooBlacktop <rhainards.z.p@gmail.com>
2026-07-17 01:39:31 -0600

    Update Supabase anonymous key in config.js
    Updated the Supabase anonymous key for configuration.

--- config.js
+++ config.js
@@ -11,4 +11,4 @@
 // ============================================================
 
 const SUPABASE_URL = https://lgmwhoryndpytnbmlpnm.supabase.co/rest/v1/;
-const SUPABASE_ANON_KEY = sb_publishable_3gO0yPueHtO-K4wFDTqzjw_tyDAN8Pf;
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnbXdob3J5bmRweXRuYm1scG5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNjM3MjYsImV4cCI6MjA5OTgzOTcyNn0.hzPeTF-KMEkxbPqXVgs2EEkM2_FHcvBlyEW78FtbXY0;
