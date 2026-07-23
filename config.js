af2d32c5f05020336e442a3e9b42983c55ba038b
BambooBlacktop <rhainards.z.p@gmail.com>
2026-07-23 02:26:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ const SUPABASE_URL = "https://lgmwhoryndpytnbmlpnm.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_3gO0yPueHtO-K4wFDTqzjw_tyDAN8Pf";
 
 // ============================================================
-// EmailJS configuration
+// 
 // ============================================================
-// Used to send an instant Email notification whenever a new
+// Used to send an instant email notification whenever a new
 // quote request is submitted, so it doesn't require manually
 // checking Supabase.
 // ============================================================
